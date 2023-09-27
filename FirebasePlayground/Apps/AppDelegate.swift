41d1263a4ccdcd804efea5e84535fa228158d189
RikutoSato <satorikuto1064@gmail.com>
2023-09-27 20:08:04 +0900

    AppDelegateを別ファイルに分けた

---
+++ FirebasePlayground/Apps/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FirebasePlayground
+//
+//  Created by RikutoSato on 2023/09/27.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
--- FirebasePlayground/Apps/FirebasePlaygroundApp.swift
+++ FirebasePlayground/Apps/FirebasePlaygroundApp.swift
@@ -8,15 +8,6 @@
 import SwiftUI
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-
-        return true
-    }
-}
-
 @main
 struct FirebasePlaygroundApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
